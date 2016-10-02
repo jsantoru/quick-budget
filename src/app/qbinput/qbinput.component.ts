0b04832e2b0483107de1263042e899bc900ad892
Joe Santoru <joesantoru@gmail.com>
2016-10-01 22:36:34 -0400

    added click handlers to adjust family members and income

--- src/app/qbinput/qbinput.component.ts
+++ src/app/qbinput/qbinput.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QBInputComponent implements OnInit {
 
+  familySize: number = 2;
+  monthlyIncome: number = 5000;
+
+  onClickFamily(direction:string) {
+    if(direction == "up") {
+      this.familySize++;
+    } else if(direction == "down") {
+      this.familySize--;
+    } else {
+      alert("BADNESS");
+    }
+  }
+
+  onClickIncome(direction:string) {
+    if(direction == "up") {
+      this.monthlyIncome = this.monthlyIncome + 100;
+    } else if(direction == "down") {
+      this.monthlyIncome = this.monthlyIncome - 100;
+    } else {
+      alert("BADNESS");
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
