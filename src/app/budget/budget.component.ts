b8527cd751be91ba02a3cf1409f0056dedfc587b
Joe Santoru <joesantoru@gmail.com>
2016-10-05 21:19:00 -0400

    set monthlyIncome and familysize on budget, removed adjustable badge from category groups

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -9,67 +9,73 @@ import {CategoryGroup} from "../budget-item/CategoryGroup";
 })
 export class BudgetComponent implements OnInit {
 
-  toBeBudgeted: number = 0;
+  @Input()
+  theMonthlyIncome: number;
 
-  categoryGroups: CategoryGroup[] = [
-    /*{categoryGroup:"Housing", budgetItems:[
-      {category:"Rent/Mortgage", budgeted: 1700},
-      {category:"Maintenance", budgeted: 300}
-    ]},
-    {category:"Food", budgetItems:[
-      {category:"Groceries", budgeted:300, subCategoryBudgetItems:[]},
-      {category:"Eating Out", budgeted:200, subCategoryBudgetItems:[]}
-    ]},
-    {category:"Cars", budgeted:500, subCategoryBudgetItems:[
-      {category:"Car Loan(s)", budgeted:300, subCategoryBudgetItems:[]},
-      {category:"Gas", budgeted:100, subCategoryBudgetItems:[]},
-      {category:"Car Insurance", budgeted:100, subCategoryBudgetItems:[]}
-    ]}*/
-    new CategoryGroup(
-      "Housing", [
-        {category:"Rent/Mortgage", budgeted: 1700},
-        {category:"Maintenance", budgeted: 300}]
-      )
-    ]
+  theFamilySize: number;
 
-  selectedCategoryGroup: CategoryGroup = this.categoryGroups[0];
+  categoryGroups: CategoryGroup[];
+
+  selectedCategoryGroup: CategoryGroup;
 
   onClickCategoryGroup(categoryGroup:CategoryGroup) {
       console.log("clicked: ", categoryGroup);
       this.selectedCategoryGroup = categoryGroup;
   }
 
-  onValueChange(categoryGroup:CategoryGroup, changedBy:number) {
-    console.log("onvaluechange: ", changedBy);
-    this.toBeBudgeted -= changedBy;
+  get totalBudgeted(): number {
+    let totalBudgeted:number = 0;
+    this.categoryGroups.forEach(function(categoryGroup:CategoryGroup) {
+      totalBudgeted += categoryGroup.budgeted;
+    });
+
+    return totalBudgeted;
   }
 
-  onClickBudgeted(budgetItem:BudgetItem, direction:string) {
-    let changeAmount: number = 100
-    if(direction == "up") {
-      budgetItem.budgeted += changeAmount;
-      this.toBeBudgeted -= changeAmount;
-    } else if(direction == "down") {
-      budgetItem.budgeted -= changeAmount;
-      this.toBeBudgeted += changeAmount;
-    } else {
-      alert("BADNESS");
+  get toBeBudgeted() : number {
+    return this.theMonthlyIncome - this.totalBudgeted;
+  }
+
+  @Input()
+  set familySize(familySize:number) {
+    if(familySize) {
+      console.log("BudgetComponent: ", familySize);
+      //this.budgetItems.push({category: "New", budgeted: 100, subCategoryBudgetItems:[]});
+      this.theFamilySize = familySize;
     }
   }
 
   @Input()
-  set familySize(arg:number) {
-    if(arg) {
-      console.log("BudgetComponent: ", arg);
-      //this.budgetItems.push({category: "New", budgeted: 100, subCategoryBudgetItems:[]});
+  set monthlyIncome(monthlyIncome:number) {
+    if(monthlyIncome) {
+      console.log("BudgetComponent: ", monthlyIncome);
+      this.theMonthlyIncome = monthlyIncome;
     }
   }
 
   constructor() {
-
   }
 
   ngOnInit() {
-  }
+    this.categoryGroups = [
+      new CategoryGroup(
+        "Housing", [
+          {category:"Rent/Mortgage", budgeted: 1700},
+          {category:"Maintenance", budgeted: 300}]
+      ),
+      new CategoryGroup(
+        "Food", [
+          {category:"Groceries", budgeted: 300},
+          {category:"Eating Out", budgeted: 200}]
+      ),
+      new CategoryGroup(
+        "Cars", [
+          {category:"Car Loan(s)", budgeted: 300},
+          {category:"Gas", budgeted: 100},
+          {category:"Car Insurance", budgeted: 100}]
+      )
+    ];
 
+    this.selectedCategoryGroup = this.categoryGroups[0];
+  }
 }
